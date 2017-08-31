e4c09e6919a50e54bec80aef2b29eb413ba05dc5
Dustin Harrell <dustin.harrell@sage.com>
2017-08-31 09:55:43 -0400

    Init commit

---
+++ Set_AWS_S3Config.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#: Script: Set_AWS_S3Config.py
+#: Description: Takes AWS Creds name for account identification, gets list of regions, puts config rules for s3 buckets.
+#: Author: Dustin Harrell
+
+import argparse
+import boto3
+
+PARSER = argparse.ArgumentParser()
+PARSER.add_argument('--PROFILE', metavar='[PROFILE]', help='AWS Cred Profile Name')
+ARGS = PARSER.parse_args()
+
+region_list = []
+
+def get_regions(profile):
+    ec2 = boto3.client('ec2', profile_name=profile)
+    regions = ec2.describe_regions()
+    for region in regions['Regions']:
+        region_list.append(region['RegionName'])
+
+def put_config(profile, region):
+    config = boto3.client('config', profile_name=profile, region_name=region)
+    rule = config.put_config_rule()
