d2c4edf362cdfac249f774207b1adaa3210f5872
Dustin Harrell <dustin.harrell@sage.com>
2018-08-24 11:34:11 -0400

    Added ability to dynamically change the affected region.

--- reshuffle.sh
+++ reshuffle.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-name="arn:aws:lambda:eu-west-1:700326549305:function:move-sqs-messages"
+name="arn:aws:lambda:$1:700326549305:function:move-sqs-messages"
 invoketype="Event"
 payload="{}"
 
 x=1
 while [ $x -le 1000 ]
 do
-  aws lambda invoke --function-name $name --invocation-type $invoketype --payload $payload --region eu-west-1 testresult.log
+  aws lambda invoke --function-name $name --invocation-type $invoketype --payload $payload --region $1 testresult.log
   echo $x
-  cmd="$(aws lambda invoke --function-name $name --invocation-type $invoketype --payload $payload --region eu-west-1 testresult.log)"
+  cmd="$(aws lambda invoke --function-name $name --invocation-type $invoketype --payload $payload --region $1 testresult.log)"
 
   x=$(( $x + 1 ))
   sleep 1
